b7b6ffc449c9d7ac2c695831b1228f6af80ade23
Jakob Runge <jaru@spreadshirt.net>
2020-12-05 00:56:11 +0100

    add amplitude

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback } from 'react';
 
-import { createSinusNode, context as audioContext } from './audio';
+import { createSinusNode, createContext } from './audio';
 
 const App = () => {
   const onClick = useCallback(() => {
-    const a = createSinusNode(audioContext, 440);
+    const audioContext = createContext();
+    const a = createSinusNode(audioContext, 440, 0.1);
 
     a.connect(audioContext.destination);
 
--- src/audio.ts
+++ src/audio.ts
@@ -1,10 +1,10 @@
 const AudioContext = window.AudioContext;
 
-export const context = new AudioContext();
+export const createContext = () => new AudioContext();
 
 const bufferSize = 4096;
 
-export const createSinusNode = (context: AudioContext, frequency: number): ScriptProcessorNode => {
+export const createSinusNode = (context: AudioContext, frequency: number, amplitude: number): ScriptProcessorNode => {
   const scriptNode = context.createScriptProcessor(bufferSize, 0, 1);
   const delta = 1 / context.sampleRate;
   let t = 0;
@@ -15,7 +15,7 @@ export const createSinusNode = (context: AudioContext, frequency: number): Scrip
 
     for (let i = 0; i < outputBuffer.length; i++) {
       t += delta;
-      channelData[i] = Math.sin(Math.PI * 2 * (t * frequency));
+      channelData[i] = Math.sin(Math.PI * 2 * (t * frequency)) * amplitude;
     }
   };
 
