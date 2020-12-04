e37724820d7420c5fcd4a9fa558bd160df949225
Jakob Runge <jaru@spreadshirt.net>
2020-12-05 00:52:18 +0100

    generate 440hz

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { createSinusNode, context as audioContext } from './audio';
 
 const App = () => {
+  const onClick = useCallback(() => {
+    const a = createSinusNode(audioContext, 440);
+
+    a.connect(audioContext.destination);
+
+    window.setTimeout(() => { a.disconnect(audioContext.destination) }, 1000);
+  }, []);
+
   return (
     <div>
-      Zebra
+      <button onClick={onClick}>
+        Zebra!
+      </button>
     </div>
   );
 }
---
+++ src/audio.ts
@@ -0,0 +1,23 @@
+const AudioContext = window.AudioContext;
+
+export const context = new AudioContext();
+
+const bufferSize = 4096;
+
+export const createSinusNode = (context: AudioContext, frequency: number): ScriptProcessorNode => {
+  const scriptNode = context.createScriptProcessor(bufferSize, 0, 1);
+  const delta = 1 / context.sampleRate;
+  let t = 0;
+
+  scriptNode.onaudioprocess = (audioProcessingEvent: AudioProcessingEvent): void => {
+    const { outputBuffer } = audioProcessingEvent;
+    const channelData = outputBuffer.getChannelData(0);
+
+    for (let i = 0; i < outputBuffer.length; i++) {
+      t += delta;
+      channelData[i] = Math.sin(Math.PI * 2 * (t * frequency));
+    }
+  };
+
+  return scriptNode;
+};
