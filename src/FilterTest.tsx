b2b2fc79ae1047fa4db7786792d424781b2ca098
Jakob Runge <jaru@spreadshirt.net>
2020-12-13 21:20:20 +0100

    look at more filters

--- src/FilterTest.tsx
+++ src/FilterTest.tsx
@@ -2,69 +2,69 @@ import React, { FC, useCallback, useMemo, useState } from 'react';
 import { range, zipWith } from 'lodash';
 
 import { createAudioContext } from './audio';
-import Series from './Series';
+import MultiSeries, { MultiSeriesData } from './MultiSeries';
 
-type FilterProps = { firstFilter: BiquadFilterNode, secondFilter: BiquadFilterNode };
+type FilterProps = { filters: Array<BiquadFilterNode> };
 
-const FilterComparison: FC<FilterProps> = ({ firstFilter, secondFilter }) => {
-  const { normalisedMagResponse } = useMemo(() => {
-    const startValue = firstFilter.frequency.value;
-    const endValue = secondFilter.frequency.value + 1;
-    const testFrequencies = Float32Array.from(range(startValue, endValue));
+const hearingStart = 20;
+const hearingEnd = 2000;
 
-    const firstMagResponse = new Float32Array(testFrequencies.length);
-    const firstPhaseResponse = new Float32Array(testFrequencies.length);
-    firstFilter.getFrequencyResponse(testFrequencies, firstMagResponse, firstPhaseResponse);
+const FilterComparison: FC<FilterProps> = ({ filters }) => {
+  const [magData, phaseData]: [MultiSeriesData, MultiSeriesData] = useMemo(() => {
+    const magData: MultiSeriesData = [];
+    const phaseData: MultiSeriesData = [];
 
-    const secondMagResponse = new Float32Array(testFrequencies.length);
-    const secondPhaseResponse = new Float32Array(testFrequencies.length);
-    secondFilter.getFrequencyResponse(testFrequencies, secondMagResponse, secondPhaseResponse);
+    const startValue = hearingStart
+    const endValue = hearingEnd + 1;
+    const testFrequencies = Float32Array.from(range(startValue, endValue, 50));
+    const magThing = new Float32Array(testFrequencies.length);
+    const phaseThing = new Float32Array(testFrequencies.length);
 
-    const combinedMagResponse = zipWith(firstMagResponse, secondMagResponse, (x, y) => (x + y));
-    const maxMagResponse = Math.max(...combinedMagResponse.map(x => Math.abs(x)));
-    const normalisedMagResponse = combinedMagResponse.map(x => x / maxMagResponse);
+    const magResponse = new Float32Array(testFrequencies.length);
+    const phaseResponse = new Float32Array(testFrequencies.length);
+    filters.forEach(filter => {
+      filter.getFrequencyResponse(testFrequencies, magResponse, phaseResponse);
 
-    const combinedPhaseResponse = zipWith(firstPhaseResponse, secondPhaseResponse, (x, y) => Math.abs(x) < Math.abs(y) ? x : y);
+      magData.push(zipWith(testFrequencies, magResponse, (x, y) => ({ x, y })));
+      phaseData.push(zipWith(testFrequencies, phaseResponse, (x, y) => ({ x, y })));
+      for (let i = 0; i < testFrequencies.length; i++) {
+        magThing[i] += magResponse[i];
+        phaseThing[i] -= phaseResponse[i];
+      }
+    });
 
-    console.table(zipWith(
-      testFrequencies, combinedMagResponse, combinedPhaseResponse,
-      (frequency, combinedMag, combinedPhase) => ({ frequency, combinedMag: combinedMag.toFixed(4), combinedPhase: combinedPhase.toFixed(4) }),
-    ));
+    magData.unshift(zipWith(testFrequencies, magThing, (x, y) => ({ x, y })));
+    phaseData.unshift(zipWith(testFrequencies, phaseThing, (x, y) => ({ x, y })));
 
-    return { normalisedMagResponse };
-  }, [firstFilter, secondFilter])
+    return [magData, phaseData];
+  }, [filters])
 
   return (
     <div>
-      <Series width={750} height={100} data={normalisedMagResponse} />
+      <h3>Amplitudes</h3>
+      <MultiSeries width={800} height={400} data={magData} />
+      <h3>Phases</h3>
+      <MultiSeries width={800} height={400} data={phaseData} />
     </div>
   );
 };
 
 const FilterTest: FC = () => {
-  const [firstFreq, setFirstFreq] = useState<number>(5);
-  const [secondFreq, setSecondFreq] = useState<number>(20);
-  const freqDelta = secondFreq - firstFreq;
-  const [q, setQ] = useState<number>(1);
-  const [filterPair, setFilterPair] = useState<[BiquadFilterNode, BiquadFilterNode] | null>(null);
+  const [frequencies, setFrequencies] = useState<Array<number>>([5, 20]);
+  const [Q, setQ] = useState<number>(1);
+  const [filters, setFilters] = useState<Array<BiquadFilterNode> | null>(null);
 
   const doThing = useCallback(() => {
     const audioContext = createAudioContext();
-    const firstFilter = new BiquadFilterNode(audioContext, { type: 'bandpass', frequency: firstFreq, Q: q });
-    const secondFilter = new BiquadFilterNode(audioContext, { type: 'bandpass', frequency: secondFreq, Q: q });
-
-    setFilterPair([firstFilter, secondFilter]);
-  }, [firstFreq, secondFreq, q, setFilterPair]);
+    setFilters(frequencies.map(frequency => (new BiquadFilterNode(audioContext, { type: 'bandpass', frequency, Q }))));
+  }, [frequencies, Q, setFilters]);
 
   return (
     <div>
-      <input type="number" value={firstFreq} onChange={(event) => setFirstFreq(Number(event.target.value))} />
-      <input type="number" value={secondFreq} onChange={(event) => setSecondFreq(Number(event.target.value))} />
-      <input type="number" value={q} onChange={(event) => setQ(Number(event.target.value))} />
+      <input pattern="[\.,+-\d]+" value={frequencies.join(', ')} onChange={(event) => setFrequencies(event.target.value.split(',').map(s => Number(s)))} />
+      <input type="number" value={Q} onChange={(event) => setQ(Number(event.target.value))} />
       <button onClick={doThing}>Do the thing!</button>
-      <button onClick={() => { setFirstFreq(firstFreq - freqDelta); setSecondFreq(secondFreq - freqDelta); }}>⬅️</button>
-      <button onClick={() => { setFirstFreq(firstFreq + freqDelta); setSecondFreq(secondFreq + freqDelta); }}>➡️</button>
-      {(filterPair !== null) && (<FilterComparison firstFilter={filterPair[0]} secondFilter={filterPair[1]} />)}
+      <FilterComparison filters={filters || []} />
     </div>
   );
 };
--- src/MultiSeries.tsx
+++ src/MultiSeries.tsx
@@ -1,27 +1,30 @@
-import React, { FC } from 'react';
-import { HorizontalGridLines, LineSeriesCanvas, LineSeriesPoint, VerticalGridLines, XYPlot, YAxis } from 'react-vis';
+import React, { FC, memo } from 'react';
+import { HorizontalGridLines, LineSeriesCanvas, LineSeriesPoint, VerticalGridLines, XAxis, XYPlot, YAxis } from 'react-vis';
 import 'react-vis/dist/style.css';
+
+export type MultiSeriesData = Array<Array<LineSeriesPoint>>;
 
 type Props = {
   width: number;
   height: number;
-  data: Array<Array<LineSeriesPoint>>;
+  data: MultiSeriesData;
   onClick?: () => unknown;
 };
 
-const MultiSeries: FC<Props> = ({ width, height, data, onClick }) => {
+const MultiSeries: FC<Props> = memo(({ width, height, data, onClick }) => {
   return (
     <div onClick={onClick}>
       <XYPlot width={width} height={height}>
         <HorizontalGridLines />
         <VerticalGridLines />
+        <XAxis />
         <YAxis />
-        {data.forEach((points, i) => (
+        {data.map((points, i) => (
           <LineSeriesCanvas key={`series-${i}`} data={points} />
         ))}
       </XYPlot>
     </div>
   );
-};
+});
 
 export default MultiSeries;
